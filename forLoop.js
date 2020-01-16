be8a27f7b166129281f8f323ce78254816ac8f8c
Enoch Nordlander <50180679+enochnord@users.noreply.github.com>
2020-01-16 13:32:46 -0500

    Fixed Comments

--- forLoop.js
+++ forLoop.js
@@ -1,12 +1,12 @@
-// for loops practice //
+// for loops practice 
 
-// print all numbers betwwen -10 and 19 //
+// print all numbers betwwen -10 and 19 
 console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19")
 for(var num = -10; num < 20; num++) {
     console.log(num)
 }
 
-// print all even numbers between 11 and 40 //
+// print all even numbers between 11 and 40 
 console.log("PRINT ALL EVEN NUMBERS BETWEEN 11 AND 40")
 for(var num = 11; num <= 40; num++) {
     if(num % 2 === 0) {
@@ -14,7 +14,7 @@ for(var num = 11; num <= 40; num++) {
     }
 }
 
-// print all odd numbers betwwen 300 and 333 //
+// print all odd numbers betwwen 300 and 333 
 console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333")
 for(var num = 300; num <= 333; num++) {
     if(num % 2 !== 0) {
@@ -22,7 +22,7 @@ for(var num = 300; num <= 333; num++) {
     }
 } 
 
-// print all numbers divisible by 5 and 3 //
+// print all numbers divisible by 5 and 3 
 console.log("PRINT ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50")
 for(var num = 5; num <=50; num++) {
     if(num % 5 === 0 && num % 3 === 0) {
